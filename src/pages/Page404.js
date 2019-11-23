6a016a1843e27a3753b02489912ad96e4ed920dd
Marcio Castillo <mecastillo00@gmail.com>
2019-11-22 21:52:37 -0500

    added 404 page

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Jumbotron, Button } from "reactstrap";
+import { Link } from "react-router-dom";
 
 export default function Page404() {
-  return <div>page not found</div>;
+  return (
+    <Jumbotron className="jumbo">
+      <h1 className="fourzerofour">404</h1>
+      <h1 className="display-3" style={{ color: "#A67EB1" }}>
+        Page Not Found
+      </h1>
+
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <Button size="lg" style={{ backgroundColor: "#A67EB1" }}>
+          Go Back Home
+        </Button>
+      </Link>
+    </Jumbotron>
+  );
 }
